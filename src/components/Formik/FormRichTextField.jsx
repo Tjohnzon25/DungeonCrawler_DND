d2a1656c2764fe7fa117b7e746c642043b4c1d7f
Timothy Johnson <60270341+Tjohnzon25@users.noreply.github.com>
2025-06-18 15:11:11 -0400

    fixed RichTextField

--- src/components/Formik/FormRichTextField.jsx
+++ src/components/Formik/FormRichTextField.jsx
@@ -69,6 +69,7 @@ const FormRichTextField = ({
             {renderControls}
           </MenuControlsContainer>
         )}
+        {...otherProps}
       >
         {children}
       </RichTextEditor>
